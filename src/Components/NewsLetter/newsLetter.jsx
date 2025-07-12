32b139e83163621aa78e65c3e7fa1d991fd59420
minhajahamad <minhajahamadmk@gmail.com>
2025-07-12 14:45:50 +0530

    updated home page

--- src/Components/NewsLetter/newsLetter.jsx
+++ src/Components/NewsLetter/newsLetter.jsx
@@ -18,7 +18,7 @@ const NewsLetter = () => {
           placeholder="name@email.com"
           className="w-[200px] md:w-[450px] lg:w-[650px] xl:w-[550px] 2xl:w-[800px] bg-white rounded-[2px] mt-6 p-2 "
         />
-        <div className="text-black bg-[#FFBA3A] font-semibold text-[16px] p-[8px] rounded-[2px] mt-6  cursor-pointer hover:bg-[#ffac12] transition-colors duration-300  ">
+        <div className="text-black bg-[#FFBA3A] font-semibold text-[13px] lg:text-[16px] p-[8px] rounded-[2px] mt-6  cursor-pointer hover:bg-[#ffac12] transition-colors duration-300  ">
           <p>Subscribe</p>
         </div>
       </div>
